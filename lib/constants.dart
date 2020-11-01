bd6cbfa42ed48a0a1a86ffcd1c8266d65c474c3c
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-01 17:58:27 +0530

    Navigation bar added

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+const List<Widget> widgetOptions = <Widget>[
+  Text(
+    'Index 0: Home',
+    style: optionStyle,
+  ),
+  Text(
+    'Index 1: Business',
+    style: optionStyle,
+  ),
+  Text(
+    'Index 2: School',
+    style: optionStyle,
+  ),
+];
--- lib/sinduHome.dart
+++ lib/sinduHome.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
-class SinduHome extends StatelessWidget {
+class SinduHome extends StatefulWidget {
+  @override
+  _SinduHomeState createState() => _SinduHomeState();
+}
+
+class _SinduHomeState extends State<SinduHome> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          
-        ),
+          body: Center(
+            child: widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.play_circle_filled_sharp),
+                label: 'Play',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_sharp),
+                label: 'Favourites',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.music_note_sharp),
+                label: 'Songs',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
+          ),
+        )
     );
   }
 }
